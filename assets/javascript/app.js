8e5f84b7a9d4bfdf95d539faf3ec14a3ee77034b
mateyo3 <mateoreyes720@gmail.com>
2017-09-24 21:47:19 -0700

    displays text from response, but not gif

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -43,38 +43,22 @@ function displayTopic() {
 	var ratingDisplay = $("<p>").text("Rating: " + rating);
 		console.log("This displays- Rating: " + rating)
 
+	// Retrieving the URL for gif
+	var gifURL = response.data[0].embed_url;
+		console.log("gifURL: " + gifURL);
+
+	// Create img tag to gif
+	var gif = $("<img>")
+
+	gif.attr("src", gifURL).attr("alt", "gif");
+
+
 	// append rating to topicDiv
 	topicDiv.append(ratingDisplay);
+	topicDiv.append(gif);
 
-	// // Storing the release year
-	// var released = response.Released;
-
-	// // Creating an element to hold the release year
-	// var pTwo = $("<p>").text("Released: " + released);
-
-	// // Displaying the release year
-	// movieDiv.append(pTwo);
-
-	// // Storing the plot
-	// var plot = response.Plot;
-
-	// // Creating an element to hold the plot
-	// var pThree = $("<p>").text("Plot: " + plot);
-
-	// // Appending the plot
-	// movieDiv.append(pThree);
-
-	// // Retrieving the URL for the image
-	// var imgURL = response.Poster;
-
-	// // Creating an element to hold the image
-	// var image = $("<img>").attr("src", imgURL);
-
-	// // Appending the image
-	// movieDiv.append(image);
-
-	// // Putting the entire movie above the previous movies
-	// $("#movies-view").prepend(movieDiv);
+	// Putting the entire movie above the previous movies
+	$("#topics-view").prepend(topicDiv);
 
 	});
 
