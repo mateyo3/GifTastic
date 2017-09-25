ef937a55ae973ebac02e6c3fe4f8e5e90acbfbab
mateyo3 <mateoreyes720@gmail.com>
2017-09-24 22:42:38 -0700

    toggle gif state code added. untested

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -108,6 +108,21 @@ function renderButtons() {
 	
 	});
 
+// Toggle gif state
+	$(".gif").on("click", function() {
+
+		var state = $(this).attr("data-state");
+			console.log("current state: " + state);
+			
+		if (state === "still") {
+			$(this).attr("src", $(this).attr("data-animate"));
+			$(this).attr("data-state", "animate");
+		} else {
+			$(this).attr("src", $(this).attr("data-still"));
+			$(this).attr("data-state", "still");
+		}
+	});
+
 $(document).on("click", ".topic", displayTopic);
 
 // Run button function
